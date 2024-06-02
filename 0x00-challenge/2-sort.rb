83b588cae1cb3e2a1b10f75f0e59ff3bd5800fa1
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-03 01:19:32 +0300

    Challenge-2

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,6 +1,6 @@
 ###
 #
-Sort integer arguments (ascending)
+#Sort integer arguments (ascending)
 #
 result = []
 ARGV.each do |arg|
@@ -10,7 +10,7 @@ ARGV.each do |arg|
   # convert to integer
   i_arg = arg.to_i
 
-insert result at the right position
+#insert result at the right position
   is_inserted = false
   i = 0
   l = result.size
